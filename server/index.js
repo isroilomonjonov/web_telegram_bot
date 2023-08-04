a4037f37b33b3af5dc4dd9e8ade46be5acad1c25
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-08-04 15:00:37 +0500

    make it better

--- server/index.js
+++ server/index.js
@@ -86,7 +86,25 @@ const botStart = () => {
 };
 botStart();
 app.post("/web-data",async(req,res)=>{
-
+ const {queryId,products}=req.body;
+ try {
+    await bot.answerWebAppQuery(queryId,{
+        type: "article",
+        id:queryId,
+        title:"Muvaffaqiyatli xarid qildingiz",
+        input_message_content:{
+            message_text:`Xaridingiz bilan tabriklayman siz ${products
+                .reduce((a, c) => a + c.price * c.quantity, 0)
+                .toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })} qiymatga ega mahsulot sotib oldingiz, ${products.map((product) =>`${c.title}-${c.quantity}x`).join(', ')}`
+        }
+    })
+    return res.status(200).json({})
+ } catch (error) {
+    return res.status(500).json({})
+ }
 })
 app.listen(process.env.PORT||8000,()=>{
     console.log("server started");
