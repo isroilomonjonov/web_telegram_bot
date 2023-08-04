2f8d69987343dcc2a5cc08238d6a22d087beaf2c
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-08-04 14:14:37 +0500

    front api added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,11 +52,21 @@ function App() {
     telegram.MainButton.show();
   };
   const onSendData = useCallback(() => {
-    return telegram.sendData(JSON.stringify(cartItems));
-  }, [cartItems]);
+    const queryId = telegram.initDataUnsave?.query_id;
+    if(queryId) {
+      fetch("http://localhost:8000/web-data",{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cartItems)
+      })
+    }else{
+    telegram.sendData(JSON.stringify(cartItems));}
+   }, [cartItems]);
   useEffect(() => {
     telegram.onEvent("mainButtonClicked", onSendData);
-   return ()=>telegram.offEvent("mainButtonClicked", onSendData);
+    return () => telegram.offEvent("mainButtonClicked", onSendData);
   }, [onSendData]);
   return (
     <>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@ const token = "6385314171:AAHMK7frezBvR3_nOfTg7dV7WO5tkEF_CDI";
 
 const bot = new TelegramBot(token, { polling: true });
 const botStart = () => {
+    bot.setMyCommands([
+        {
+            command: "/start",
+            description: "Start"
+        },
+        {
+            command: "/courses",
+            description:"Courselar"
+        }
+    ])
   bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
@@ -26,18 +36,43 @@ const botStart = () => {
         }
       );
     }
-    console.log(msg.web_app_data?.data,"ddddddddddddddddddddddddd");
+    if (text === "/courses") {
+        await bot.sendMessage(
+            chatId,
+            "Assalom Alekum Web Botimizga hush kelibsiz!",
+            {
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: "Kurslarni ko'rish",
+                      web_app: {
+                        url: "https://web-telegram-bot-xcmb.vercel.app/",
+                      },
+                    },
+                  ],
+                ],
+              },
+            }
+          );
+    }
+
     if (msg.web_app_data?.data) {
       try {
-        console.log("aaaaaaaaaaaaaaaaa");
         const data = JSON.parse(msg.web_app_data.data);
         await bot.sendMessage(chatId, `Siz sotib olgan kurslar, `);
         for (item of data) {
+          await bot.sendPhoto(chatId, item.Image);
           await bot.sendMessage(chatId, `${item.title}-${item.quantity}x`);
         }
-       return await bot.sendMessage(
+        return await bot.sendMessage(
           chatId,
-          `Umumiy narx, ${data.reduce((a, c) => a + c.price * c.quantity, 0)} `
+          `Umumiy narx, ${data
+            .reduce((a, c) => a + c.price * c.quantity, 0)
+            .toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })} `
         );
       } catch (error) {
         console.log(error);
